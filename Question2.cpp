8b9eab4e262e817cc2035033339eae85a72ea0c5
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-07 09:57:23 -0600

    Add files via upload

---
+++ Question2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double c;
+    
+    cout<<"Enter Centimeters here ";
+    cin>>c;
+    cout<<"Meter is "<<c/100<<"m"<<endl;
+    cout<<"Kilometer is "<<c/100000<<"km"<<endl;
+    
+    
+    return 0;
+}
+
