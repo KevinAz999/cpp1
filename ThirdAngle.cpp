e734a5ca4937e6cb2cf35eeffece80ff684c53f4
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-07 10:16:06 -0600

    Add files via upload

---
+++ ThirdAngle.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    double a,b;
+    
+    cout<<"Enter First Angle ";
+    cin>>a;
+    cout<<"Enter Second Angle";
+    cin>>b;
+    
+    cout<<"The Third Angle is "<<180-(a+b)<<" and the sum of all the angles in the triangle equal to 180"<<endl;
+    
+    
+    
+    
+    return 0;
+}
