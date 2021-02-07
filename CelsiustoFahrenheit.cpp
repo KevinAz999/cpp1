f85d147ec9d4b07c29b15c760f695ac20c46c0f8
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-07 10:04:38 -0600

    Add files via upload

---
+++ CelsiustoFahrenheit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double c;
+    
+    cout<<"Enter Temp in Celsius ";
+    cin>>c;
+    cout<<"The Temp in Fahrenheit is "<<c*9/5+32<<"f"<<endl;
+    return 0;
+}
+
