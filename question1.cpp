a78fe0c3ab47e7b6ae79c73163bb538e115bc049
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-07 09:43:35 -0600

    Add files via upload

---
+++ question1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double r;
+    
+    cout<<"Enter the radius: \t";
+    cin>>r;
+    cout<<"Diameter is: \t\t"<<r*2<<endl;
+    cout<<"Circumference is: \t"<<2*3.14*r<<endl;
+    cout<<"Area of the circle: \t"<<3.14*r*r<<endl;
+    
+   
+    
+    
+    
+    return 0;
+}
