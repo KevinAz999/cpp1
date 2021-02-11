19f9ebe0b805757ba056fcbb61d6a6f5b403c817
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-11 16:25:09 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,18 +2,20 @@
 #include <iostream>
 
 using namespace std;
-int main(){
-    
-    double l,w;
-    
-    cout<<"Enter Length=";
-    cin>>l;
-    cout<<"Enter Width=";
-    cin>>w;
-    
-    cout<<"The Area of the numbers you imputed is "<<l*w<<endl;
-    
 
+int main() {
+    
+    double a,b;
+    
+    cout<<"Enter First Angle ";
+    cin>>a;
+    cout<<"Enter Second Angle";
+    cin>>b;
+    
+    cout<<"The Third Angle is "<<180-(a+b)<<" and the sum of all the angles in the triangle equal to 180"<<endl;
+    
+    
+    
     
     return 0;
 }
