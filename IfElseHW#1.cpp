c7d9c233346bb453d0ccea85a89b8f00c42d669f
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-11 16:18:35 -0600

    Add files via upload

---
+++ IfElseHW#1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  
+    int x = 10;
+    int y = 20;
+    
+    cout<<"Imputs, 10, and 20."<<endl;
+    
+    if(x != y){
+        cout<<"a"<<endl;
+    }else if(x == y){
+        cout<<"b"<<endl;
+        }
+    
+    
+    return 0;
+}
