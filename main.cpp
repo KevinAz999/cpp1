f3f8096ce3ed4834fb8454eb192e4f714a676a2c
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-11 16:23:33 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    
+    double l,w;
+    
+    cout<<"Enter Length=";
+    cin>>l;
+    cout<<"Enter Width=";
+    cin>>w;
+    
+    cout<<"The Area of the numbers you imputed is "<<l*w<<endl;
+    
+
+    
+    return 0;
+}
