a5f20ae3b501b57f235d06826b60077ec1b0d94e
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-02-20 07:42:00 -0600

    Update IfElseHWNumber5Updated.cpp

--- IfElseHWNumber5Updated.cpp
+++ IfElseHWNumber5Updated.cpp
@@ -8,11 +8,12 @@ int main() {
     
     cout<<"Enter a number to find out if it is even or odd: "<<endl;
     cin>>number;
-    
-    if(number%2 == 0){
-        cout<<"The number is an even number."<<endl;
-    }else{
+    if(number%3 == 0){
         cout<<"This number is an odd number"<<endl;
+    }if(number%2 == 0){
+        cout<<"The number is an even number."<<endl;
+    {else{
+        cout<<"This is not a number"<<endl;
     }
     
     
