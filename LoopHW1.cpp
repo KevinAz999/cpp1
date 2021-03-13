fc7a5b202c8432590fdcf88ca439d2d4f239c256
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-03-12 18:29:20 -0600

    Create LoopHW1.cpp

---
+++ LoopHW1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int i = 0;
+  int n;
+  
+  cout<<"Enter max number: ";
+  cin>>n;
+  
+  while(n != i){
+      cout<<i<<" ";
+      i++;
+  }
+  
+  
+}
