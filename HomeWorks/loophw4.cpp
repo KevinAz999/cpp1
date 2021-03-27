e8404a1f07e657a234510871b3496f66e83c92e9
Lkhagva-erdene <anidas.com@gmail.com>
2021-03-27 17:17:06 +0300

    Update loophw4.cpp (#1)

--- HomeWorks/loophw4.cpp
+++ HomeWorks/loophw4.cpp
@@ -3,20 +3,14 @@
 using namespace std;
 
 int main() {
-
-    int i;
     int n;
+    cout<<"Print all even numbers until: ";
+    cin>>n;
 
-    cout << "Print all even numbers until: ";
-    cin >> n;
-
-    for(i = 2; i <= n; i++) {
-        if(i % 2 == 0) {
-
-            cout << i << " ";
-
+    for(int i=2; i<=n; i++) {
+        if(i%2 == 0) {
+            cout <<i<<" ";
         }
     }
-
     return 0;
 }
