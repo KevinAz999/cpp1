b99322ddc36032b972901007433c4e0b77a3979a
Lkhagva-erdene <anidas.com@gmail.com>
2021-03-27 17:12:43 +0300

    Update loophw5.cpp

--- HomeWorks/loophw5.cpp
+++ HomeWorks/loophw5.cpp
@@ -4,16 +4,14 @@ using namespace std;
 
 int main() {
 
-    int i, n;
+    int n;
 
-    cout << "Print all odd numbers untill: ";
-    cin >> n;
+    cout<<"Print all odd numbers untill: ";
+    cin>>n;
 
-    for(i = 1; i <= n; i++) {
-        if(i % 3 == 0) {
-
-            cout << i << " ";
-
+    for(int i=1; i<=n; i++){
+        if(i%2 == 1){
+            cout<<i<<" ";
         }
     }
 
