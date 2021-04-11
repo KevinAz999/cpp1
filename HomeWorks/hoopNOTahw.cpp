5a7b8d4962d6b0035f2b279156106782036e54f0
KevinAz999 <78311362+KevinAz999@users.noreply.github.com>
2021-04-11 08:40:47 -0500

    Create hoopNOTahw.cpp

---
+++ HomeWorks/hoopNOTahw.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  char chara=97;
+  
+  
+  while(chara <= 122){
+      cout<<chara<<" ";
+      chara++;
+  }
+  cout<<"All lowercase letters";
+  
+  
+  return 0;
+}
